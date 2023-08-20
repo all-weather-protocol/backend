a1719761c08da81ad7a5c3711f299a755e024b04
davidtnfsh <davidtnfsh@gmail.com>
2023-08-20 20:59:47 +0800

    [fix](amount): should use bignumber.from

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 const port = 3002;
 const provider = new ethers.providers.JsonRpcProvider(process.env.API_URL);
-const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);    
+const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
 
 // Middleware to parse JSON requests
@@ -36,7 +36,7 @@ app.get('/pendle/zapIn', async (req, res) => {
       throw new Error('Invalid input');
     }
 
-    const pendleZapInData = await getPendleZapInData(chainId, poolAddress, ethers.utils.parseEther(amount), slippage, tokenInAddress);
+    const pendleZapInData = await getPendleZapInData(chainId, poolAddress, ethers.BigNumber.from(amount), slippage, tokenInAddress);
     res.json(pendleZapInData);
   } catch (error) {
     console.error(error);
@@ -60,42 +60,21 @@ app.listen(port, () => {
 });
 
 async function getPendleZapInData(chainId, poolAddress, amount, slippage, tokenInAddress) {
-    const router = Router.getRouterWithKyberAggregator({
-      chainId: chainId,
-      provider,
-      signer,
-    });
-  
-    const GLP_POOL_ADDRESS = toAddress(poolAddress);
-    const TOKEN_IN_ADDRESS = toAddress(tokenInAddress);
-    return await router.addLiquiditySingleToken(
-      GLP_POOL_ADDRESS,
-      TOKEN_IN_ADDRESS,
-      amount,
-      slippage,
-      { method: 'extractParams' }
-    );
-  }
-  async function getPendleZapOutData(chainId, poolAddress, tokenOutAddress, amount, slippage) {
-    const marketContract = new MarketEntity(toAddress(poolAddress), {
-      chainId: chainId,
-      provider,
-      signer: signer
-    });
-    const router = Router.getRouterWithKyberAggregator({
-      chainId: chainId,
-      provider,
-      signer,
-    });
-    // TODO(david): ask pendle team about this. Is it possible to extract Param before approving contract?
-    // await marketContract.approve(router.address, amount).then((tx)=> tx.wait());
-    // await marketContract.approve(router.address, ethers.BigNumber.from('115792089237316195423570985008687907853269984665640564039457')).then((tx) => tx.wait());
-  
-    return await router.removeLiquiditySingleToken(
-      toAddress(poolAddress),
-      amount,
-      toAddress(tokenOutAddress),
-      slippage,
-      { method: 'extractParams' }
-    );
-  }
+  const provider = new ethers.providers.JsonRpcProvider(process.env.API_URL);
+  const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  const router = Router.getRouterWithKyberAggregator({
+    chainId: chainId,
+    provider,
+    signer,
+  });
+
+  const GLP_POOL_ADDRESS = toAddress(poolAddress);
+  const TOKEN_IN_ADDRESS = toAddress(tokenInAddress);
+  return await router.addLiquiditySingleToken(
+    GLP_POOL_ADDRESS,
+    TOKEN_IN_ADDRESS,
+    amount,
+    slippage,
+    { method: 'extractParams' }
+  );
+}
